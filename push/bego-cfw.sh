e8a48b89d3f7fd3c35c73dfaf9c278ca28f0de88
ZyCromerZ <neetroid97@gmail.com>
2021-03-08 07:43:17 +0700

    up for 'x01bd-main-q'
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ push/bego-cfw.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+git checkout master
+
+if [ ! -z "$1" ];then
+    ListBranch="$1"
+else
+    ListBranch="begonia-cfw-stock"
+fi
+
+for Branch in $ListBranch
+do
+    git checkout master 
+    git branch -D $Branch 
+    git checkout -b $Branch 
+    git commit --amend -s -m "up for '$Branch'"
+done
+
+if [ ! -z "$2" ];then
+    repo="$2"
+else
+    repo="zyc"
+fi
+git push -f "$repo" $ListBranch
